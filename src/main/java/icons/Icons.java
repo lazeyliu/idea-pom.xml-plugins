bd93b3638e722fe6b307c8df5396d0e149c023ee
lazeyliu <liuzeyuan@yiche.com>
2024-04-16 15:52:48 +0800

    格式化

--- src/main/java/icons/Icons.java
+++ src/main/java/icons/Icons.java
@@ -5,12 +5,8 @@ import com.intellij.openapi.util.IconLoader;
 import javax.swing.*;
 
 /**
- * <p>
- *
- * </p>
- *
  * @author liuyang
- * @date 2020/2/3 周一 19:25:00
+ * @since 2020/2/3 周一 19:25:00
  * @since 1.0.0
  */
 public interface Icons {
